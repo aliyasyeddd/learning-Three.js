e877593af7e94cea23e006b7d6df6146bf1cb179
aliya mahaboob <122204472+aliyasyeddd@users.noreply.github.com>
2026-05-20 10:07:08 +0530

    creating a scene

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# learning-Three.js
-Learning Three.js
+## Learning Three js
---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import * as THREE from "three";
+
+function App() {
+  // Canvas
+  const canvas = document.querySelector("canvas.webgl");
+
+  // Scene
+  const scene = new THREE.Scene();
+
+  // Object (Adding a red cube, square in this case)
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshBasicMaterial({ color: 0xFF0000 });
+  const mesh = new THREE.Mesh(geometry, material);
+
+  scene.add(mesh); //gotta add the mesh to the scene
+
+  // Sizes of objects
+  const sizes = {
+    width: 800,
+    height: 600,
+  };
+
+  // Camera
+  const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+  camera.position.z = 3; // Takes the camera in front of the object
+  scene.add(camera); //gotta add the camera to the scene
+
+  // Renderer
+  const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+  });
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.render(scene, camera); // Have to render both or else you're cooked
+
+  return (
+    <>
+      <h1>Learning Three.js</h1>
+    </>
+  );
+}
+
+export default App;
